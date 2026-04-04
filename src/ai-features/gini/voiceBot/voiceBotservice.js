f96bcc9d94b02abb430c11cf8a6147925ec9fff3
Ajay <ajaysk3433@gmail.com>
2026-04-04 12:28:55 +0530

    feat: add SarvamAI integration for text-to-speech conversion in voiceBotService

--- src/ai-features/gini/voiceBot/voiceBotservice.js
+++ src/ai-features/gini/voiceBot/voiceBotservice.js
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 import { errorMessage } from "../../../../error.js";
-import { maxLength } from "zod";
+
+import { SarvamAIClient } from "sarvamai";
+
 let client;
 
 try {
@@ -10,13 +12,36 @@ try {
 } catch {
   errorMessage.push("API_KEY OPENAI_API_KEY required");
 }
+
+const sarvamClient = new SarvamAIClient({
+  apiSubscriptionKey: process.env.SARVAM_API_KEY,
+});
+
 export const voiceBotService = async (message) => {
+  console.log("voiceBotService");
   const response = await client.responses.create({
     model: "gpt-4o-mini",
     input: message,
     max_output_tokens: 100,
   });
 
-  console.log(response.output_text);
-  return { role: "assistant", content: response.output_text };
+  const sarvamResponse = await sarvamClient.textToSpeech.convert({
+    text: response.output_text,
+    target_language_code: "hi-IN",
+    speaker: "arya",
+    pace: 1.1,
+    speech_sample_rate: 22050,
+    enable_preprocessing: true,
+    model: "bulbul:v2",
+    dict_id: "p_c7b89ab3", // Pronunciation dictionary
+  });
+
+  // console.log(sarvamResponse);
+
+  // console.log(response.output_text);
+  return {
+    role: "assistant",
+    content: response.output_text,
+    audio: sarvamResponse.audios[0],
+  };
 };
